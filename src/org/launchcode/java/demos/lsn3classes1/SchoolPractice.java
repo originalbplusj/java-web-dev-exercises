456c3eb34d7806c96996a34d50c88b6883730d20
originalbplusj <originalbplusj@yahoo.com>
2022-03-24 15:45:40 -0500

    Chapter 4 exercises

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,5 +3,11 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student studentBeth = new Student();
+
+        studentBeth.setName("Beth");
+        studentBeth.setStudentId(55207);
+        studentBeth.setNumberOfCredits(1);
+        studentBeth.setGpa(4.0);
     }
 }
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -10,4 +10,40 @@ public class Student {
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName){
+        name = aName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int aStudentId) {
+        studentId = aStudentId;
+    }
+
+    public int getNumberOfCredits(){
+        return numberOfCredits;
+    }
+
+    public int setNumberOfCredits(int aNumberOfCredits){
+        numberOfCredits = aNumberOfCredits;
+        return aNumberOfCredits;
+    }
+
+    public double getGpa(){
+        return gpa;
+    }
+
+    public double setGpa(double aGpa){
+        gpa = aGpa;
+        return aGpa;
+    }
+
+
+
 }
